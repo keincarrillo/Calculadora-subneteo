b961b3cf75d098514af6a70264675e91238d81d7
KeinPC <kein.carrillo.escom@gmail.com>
2025-05-29 22:31:47 -0600

    refactor<routes.ts>: Modificacion de la ruta para mandalo al Front

--- Backend/src/api/api.ts
+++ Backend/src/api/api.ts
@@ -1,15 +1,14 @@
 import express from "express";
-import cors from "cors";
 import router from "../routes/routes";
+import cors from "cors";
 
 const app = express();
+
 app.use(express.json());
 app.use(cors());
 
-const PORT = 3000;
-
 app.use("/", router);
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
 });
--- Backend/src/routes/routes.ts
+++ Backend/src/routes/routes.ts
@@ -1,19 +1,35 @@
-import { Router } from "express";
+import { Router} from "express";
+import type { Request, Response } from "express";
+import { calcularSubneteo } from "../utils/subnetCalculos";
+
 const router = Router();
 
-router.get("/", (req, res) => {
+router.get("/", (req: Request, res: Response) => {
   res.json({
     ok: true,
     routes: ["api/subnet"],
   });
 });
 
-router.post("/api/subnet", (req, res) => {
-  res.json({
-    ok: true,
-    subnet: {ip: "ok"},
-  });
-});
+router.post(
+  "/api/subnet",
+  (
+    req: Request<{}, {}, { ip: string; mascara: number; mascaraNueva?: number }>,
+    res: Response
+  ) => {
+    const { ip, mascara, mascaraNueva } = req.body;
 
+    if (!ip || typeof mascara !== "number") {
+      return res.status(400).json({ error: "Faltan datos requeridos" });
+    }
+
+    const resultado = calcularSubneteo(ip, mascara, mascaraNueva);
+
+    return res.json({
+      ok: true,
+      resultado,
+    });
+  }
+);
 
 export default router;
